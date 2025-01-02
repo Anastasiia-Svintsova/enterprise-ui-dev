a7033aeb2eea0ef7d625f7970f084d8fc4788026
Anastasiia Lantratova <svintsova.nastya@gmail.com>
2025-01-02 22:58:05 +0100

    test: counter

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,6 +1,7 @@
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
+import { render } from './test/utilities';
 
 test('it should render the component', () => {
   render(<Counter />);
@@ -20,9 +21,24 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  const initialCount = 2000;
+  render(<Counter initialCount={initialCount} />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent(initialCount.toString());
+});
 
-test.todo(
-  'it should reset the count when the "Reset" button is pressed',
-  async () => {},
-);
+test('it should reset the count when the "Reset" button is pressed', async () => {
+  const { user } = render(<Counter initialCount={65} />);
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+  const resetButton = screen.getByRole('button', { name: /reset/i });
+  const currentCount = screen.getByTestId('current-count');
+
+  await user.click(incrementButton);
+  await user.click(incrementButton);
+
+  expect(currentCount).toHaveTextContent('67');
+
+  await user.click(resetButton);
+  expect(currentCount).toHaveTextContent('0');
+});
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,5 +1,10 @@
-export {};
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-/**
- * For a complete example, see: test/utilities.ts
- */
+export const render = (
+  ui: React.ReactElement,
+  options?: Parameters<typeof renderComponent>[1],
+) => ({
+  ...renderComponent(ui, options),
+  user: userEvent.setup(),
+});
